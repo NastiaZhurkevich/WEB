10495eb7a38ab6929bcf9a096272cca47db844e9
zhora_95 <sh_nastia95@mail.ru>
2020-11-01 19:35:19 +0300

    Лабаротарная работа 2

--- WebApp_1/Controllers/HomeController.cs
+++ WebApp_1/Controllers/HomeController.cs
@@ -1,37 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using WebApp_1.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WebApp_1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private List<ListDemo> _listDemo;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            _listDemo = new List<ListDemo>
+            {
+            new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+            new ListDemo{ ListItemValue=2, ListItemText="Item 2"},
+            new ListDemo{ ListItemValue=3, ListItemText="Item 3"}
+            };
         }
-
         public IActionResult Index()
         {
+            ViewData["Text"] = "Лабораторная работа 2";
+            ViewData["Lst"] = new SelectList(_listDemo,"ListItemValue","ListItemText");
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+       
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    }
+
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
+
     }
 }
